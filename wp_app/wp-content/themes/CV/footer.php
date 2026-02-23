2c8fa18ba28001a474207dfb383e9ef5c129c51a
Caroline BIEHLER <70326049+Caroline1609@users.noreply.github.com>
2026-02-23 15:45:37 +0100

    upload

---
+++ wp_app/wp-content/themes/CV/footer.php
@@ -0,0 +1,12 @@
+<!-- DEBUT FOOTER -->
+</main>
+
+<footer>
+    Copyright CRM 2026
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
+<!-- FIN FOOTER -->
---
+++ wp_app/wp-content/themes/CV/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function cb_add_thumbnails(){
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'cb_add_thumbnails');
+
+function navbar_setup(){
+    add_theme_support('title-tag');
+    register_nav_menus( array(
+        'menu-principal' => 'Menu de Navigation Principal',
+    ) );
+}
+add_action( 'after_setup_theme', 'navbar_setup' );
+
